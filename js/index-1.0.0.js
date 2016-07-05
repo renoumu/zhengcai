b51890864d3bedda31656412c7a18e9274dba56b
renoumu <renoumu@126.com>
2016-07-06 00:08:13 +0800

    ‘del’

---
+++ js/index-1.0.0.js
@@ -0,0 +1,21 @@
+$(function() {
+	$('.nav_top_list>li').mousemove(function(event) {
+		$(this).css('color', 'white');
+	});
+	$('.nav_top_list>li').mouseout(function(event) {
+		$(this).css('color', '#bbbbbc');
+	});
+
+	$('#nav_list>li').mousemove(function(event) {
+		$(this).css('color', 'red');;
+	});
+	$('#nav_list>li').mouseout(function(event) {
+		$(this).css('color', '');
+	});
+	$('#icon_ul>li').mousemove(function(event) {
+		$(this).css('color', '#979696');
+	});
+	$('#icon_ul>li').mouseout(function(event) {
+		$(this).css('color', 'red');
+	});
+});
